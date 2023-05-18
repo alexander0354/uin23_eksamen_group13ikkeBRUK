5987eff57dd1dd3f065a211e854f45cd0e2a7666
seka122 <111863067+seka122@users.noreply.github.com>
2023-05-18 13:56:47 +0200

    mygames
    importert sass

--- src/components/MyGames.js
+++ src/components/MyGames.js
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import GameCard from "./GameCard";
+import "../sass/mygames.scss";
 
 const MyGames = () => {
   const [games, setGames] = useState([]);
